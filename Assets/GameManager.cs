cf6420fefd36b3595c8941f2832a26e8f069e30d
ADigitalMindNet <50277107+ADigitalMindNet@users.noreply.github.com>
2019-05-24 13:13:06 +0100

    Initial
    Initial

---
+++ Assets/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public GameObject theCube;
+	public LayerMask groundLayer;
+	public Material canPlaceMaterial;
+	public Material cantPlaceMaterial;
+
+	private ObjectScript cubeScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		cubeScript = theCube.GetComponent<ObjectScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		Renderer cubeRender = theCube.GetComponent<Renderer>();
+		Bounds cubeBounds = cubeRender.bounds;
+
+		if (Physics.Raycast(ray,out hit,100f,groundLayer))
+		{
+			theCube.SetActive(true);
+			cubeScript.SetCanPlace(hit.normal.y);
+
+			theCube.transform.position = hit.point + new Vector3(0, cubeBounds.extents.y, 0);
+
+			if (Input.GetMouseButtonDown(0) && hit.normal.y == 1)
+			{
+				cubeScript.PlaceObject(hit.point, Quaternion.identity);
+			}
+
+		} else
+		{
+			theCube.SetActive(false);
+		}
+
+
+
+    }
+}
---
+++ Assets/ObjectScript.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScript : MonoBehaviour
+{
+	public Material canPlaceMaterial;
+	public Material cantPlaceMaterial;
+	public GameObject objectToBePlaced;
+
+	private bool _canPlace = false;
+	private Renderer objectsRenderer;
+	private Bounds objectBounds;
+	private bool _collidingWithBuilding = false;
+	private bool _objectPlaced = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		objectsRenderer = this.GetComponent<Renderer>();
+		objectBounds = objectsRenderer.bounds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		SetObjectMaterial();
+    }
+
+	private void SetObjectMaterial()
+	{
+		if (_canPlace && !_collidingWithBuilding || _objectPlaced)
+		{
+			objectsRenderer.material = canPlaceMaterial;
+		} else
+		{
+			objectsRenderer.material = cantPlaceMaterial;
+		}
+	}
+
+	public void SetCanPlace(float normalValue)
+	{
+		if(normalValue == 1)
+		{
+			_canPlace = true;
+		} else
+		{
+			_canPlace = false;
+		}
+	}
+
+	public void SetObjectPlaced()
+	{
+		_objectPlaced = true;
+	}
+
+	public void PlaceObject(Vector3 place, Quaternion rotation)
+	{
+		if (_canPlace && !_collidingWithBuilding)
+		{
+			GameObject createdObject =  Instantiate(objectToBePlaced, place + new Vector3(0, objectBounds.extents.y, 0), rotation);
+			createdObject.GetComponent<ObjectScript>().SetCanPlace(1);
+			createdObject.GetComponent<ObjectScript>().SetObjectPlaced();
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Building")
+		{
+			_collidingWithBuilding = true;
+		}
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Building")
+		{
+			_collidingWithBuilding = true;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Building")
+		{
+			_collidingWithBuilding = false;
+		}
+	}
+}
